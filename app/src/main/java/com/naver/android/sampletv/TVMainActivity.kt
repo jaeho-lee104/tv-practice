c0615cbbb138b04d955ccbb271ea5aa46af67b56
leejaeho <jaeho.lee104@navercorp.com>
2020-08-07 19:26:16 +0900

    project init

---
+++ app/src/main/java/com/naver/android/sampletv/TVMainActivity.kt
@@ -0,0 +1,11 @@
+package com.naver.android.sampletv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TVMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tv_main)
+    }
+}
